7a507aa35bb029f2a0a9b925a6680f77eb5bd489
alaniv <ivoalani@outlook.com>
2019-09-14 19:23:52 -0300

    mejoras en shader.h

--- src/shader.h
+++ src/shader.h
@@ -1,74 +1,38 @@
 #ifndef SHADER_H
 #define SHADER_H
 
-#include <glad/glad.h>
-
 #include <string>
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <stdexcept>  
+
 
 class Shader {
     
 public:
     GLuint ID;
-    Shader(const char* vertexPath, const char* fragmentPath){
-        std::string vertexCode;
-        std::string fragmentCode;
-        std::stringstream vShaderStream, fShaderStream;
-        
-        std::ifstream vShaderFile;
-        std::ifstream fShaderFile;
-        
-        vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-        fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-        try {
-            vShaderFile.open(vertexPath);
-            fShaderFile.open(fragmentPath);
+    Shader(const char* vertexPath, const char* fragmentPath){       
+        std::string vShaderCode = readShaderFile(vertexPath);
+        std::string fShaderCode = readShaderFile(fragmentPath);
+        const char* vShaderCodeCstr = vShaderCode.c_str();
+        const char* fShaderCodeCstr = fShaderCode.c_str();
 
-            vShaderStream << vShaderFile.rdbuf();
-            fShaderStream << fShaderFile.rdbuf();
-
-            vShaderFile.close();
-            fShaderFile.close();
-
-            vertexCode   = vShaderStream.str();
-            fragmentCode = fShaderStream.str();
-        } catch (std::ifstream::failure e){
-            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
-        }
-        
-        std::cout << vertexCode << std::endl;
-        std::cout << fragmentCode << std::endl;
-        
-        const char* vShaderCode = vertexCode.c_str();
-        const char * fShaderCode = fragmentCode.c_str();
-
-        GLuint vertex = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertex, 1, &vShaderCode, NULL);
-        glCompileShader(vertex);
-        checkCompileErrors(vertex, "VERTEX");
+        GLuint vertex = compileVertexShader(vShaderCodeCstr);
 
         GLuint fragment = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fragment, 1, &fShaderCode, NULL);
+        glShaderSource(fragment, 1, &fShaderCodeCstr, NULL);
         glCompileShader(fragment);
-        checkCompileErrors(fragment, "FRAGMENT");
+        checkCompilerErrorsShader(fragment, "FRAGMENT");
 
-        ID = glCreateProgram();
-        glAttachShader(ID, vertex);
-        glAttachShader(ID, fragment);
-        glLinkProgram(ID);
-        checkCompileErrors(ID, "PROGRAM");
-        
-        glDeleteShader(vertex);
-        glDeleteShader(fragment);
+        linkProgramAndDeleteShaders(vertex, fragment);
     }
 
     void use() { 
         glUseProgram(ID); 
     }
 
-   void setBool(const std::string &name, bool value) const {         
+    void setBool(const std::string &name, bool value) const {         
         glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value); 
     }
 
@@ -82,25 +46,57 @@ public:
 
 private:
 
-    void checkCompileErrors(GLuint shader, std::string type){
+    GLuint compileVertexShader(const char * vShaderCodeCstr){
+        GLuint vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, &vShaderCodeCstr, NULL);
+        glCompileShader(vertex);
+        checkCompilerErrorsShader(vertex, "VERTEX");
+        return vertex;
+    }
+    void linkProgramAndDeleteShaders(GLuint vertex, GLuint fragment){
+        ID = glCreateProgram();
+        glAttachShader(ID, vertex);
+        glAttachShader(ID, fragment);
+        glLinkProgram(ID);
+        checkCompilerErrorsProgram(ID);
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+    }
+    
+    std::string readShaderFile(const char* path){
+        std::ifstream shaderFile;
+        std::stringstream shaderStream;
+        shaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        try {
+            shaderFile.open(path);
+            shaderStream << shaderFile.rdbuf();
+            shaderFile.close();
+        } catch (const std::exception &e){
+            throw std::runtime_error(std::string("ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ"));
+        }
+        if(shaderStream.str() == ""){
+            throw std::runtime_error(std::string("ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ_EMPTY"));
+        }
+        return shaderStream.str();
+    }
+
+    void checkCompilerErrorsShader(GLuint shader, std::string type){
         int success;
         char infoLog[1024];
-        if (type != "PROGRAM"){
-            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-            if (!success){
-                glGetShaderInfoLog(shader, 1024, NULL, infoLog);
-                std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << std::endl;
-                std::cout << infoLog << std::endl; 
-                std::cout << "-- --------------------------------------------------- -- " << std::endl;
-            }
-        } else {
-            glGetProgramiv(shader, GL_LINK_STATUS, &success);
-            if (!success){
-                glGetProgramInfoLog(shader, 1024, NULL, infoLog);
-                std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << std::endl;
-                std::cout << infoLog << std::endl; 
-                std::cout << "-- --------------------------------------------------- -- " << std::endl;
-            }
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+        if (!success){
+            glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+            throw std::runtime_error(std::string("ERROR::SHADER_COMPILATION_ERROR of type: ") + type);
+        }
+    }
+    
+    void checkCompilerErrorsProgram(GLuint shader){
+        int success;
+        char infoLog[1024];
+        glGetProgramiv(shader, GL_LINK_STATUS, &success);
+        if (!success){
+            glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+            throw std::runtime_error(std::string("ERROR::PROGRAM_LINKING_ERROR"));
         }
     }
 };
